fc0c85a74131e5503fa7cd53a5cf9494732f919a
tohsaka888 <156132264@qq.com>
2022-05-11 16:50:25 +0800

    feat: 替换rect为div

--- src/d3_components/point.ts
+++ src/d3_components/point.ts
@@ -1,4 +1,4 @@
-import { D3CANVAS } from "./type"
+import { D3CANVAS, Point } from "./type"
 
 const createPoint = (container: D3CANVAS, x: string | number, y: string | number, color: string) => {
   const pointContainer = container?.append('g')
@@ -10,15 +10,18 @@ const createPoint = (container: D3CANVAS, x: string | number, y: string | number
     .attr('fill', color)
 }
 
-const createRect = (container: D3CANVAS, x: string | number, y: string | number, color: string) => {
-  const rectContainer = container?.append('g')
-  rectContainer
-    ?.append('rect')
+const createRect = (container: D3CANVAS, x: string | number, y: string | number, color: string, point: Point) => {
+  const rectContainer = container
+    ?.append('foreignObject')
     .attr('x', x)
     .attr('y', y)
-    .attr('width', '4%')
-    .attr('height', '15%')
-    .attr('fill', color)
+    .attr('width', '24px')
+    .attr('height', '246px')
+  rectContainer
+    ?.append('xhtml:div')
+    .attr('xmlns', 'http://www.w3.org/1999/xhtml')
+    .classed(color, true)
+    .text(point.name)
 }
 
 const createPointInfo = (container: D3CANVAS, x: string | number, y: string | number, info: string) => {
@@ -36,19 +39,19 @@ const createPointInfo = (container: D3CANVAS, x: string | number, y: string | nu
     .text(info)
 }
 
-const createRectInfo = (container: D3CANVAS, x: string | number, y: string | number, info: string) => {
-  container
-    ?.append('text')
-    .attr('x', x)
-    .attr('y', y)
-    .attr('font-size', '14px')
-    .attr('font-weight', 'bold')
-    .attr("dominant-baseline", "central")
-    .style('font-family', 'PingFang SC')
-    .attr('fill', '#fff')
-    .attr('writing-mode', 'tb')
-    .attr('length-adjust', 'spacing')
-    .text(info)
-}
+// const createRectInfo = (container: D3CANVAS, x: string | number, y: string | number, info: string) => {
+// container
+//   ?.append('text')
+//   .attr('x', x)
+//   .attr('y', y)
+//   .attr('font-size', '14px')
+//   .attr('font-weight', 'bold')
+//   .attr("dominant-baseline", "central")
+//   .style('font-family', 'PingFang SC')
+//   .attr('fill', '#fff')
+//   .attr('writing-mode', 'tb')
+//   .attr('length-adjust', 'spacing')
+//   .text(info)
+// }
 
-export { createPoint, createPointInfo, createRect, createRectInfo }
+export { createPoint, createPointInfo, createRect }
