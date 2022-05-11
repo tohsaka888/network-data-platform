04f83b33ea44c28009c7561696e25d4444033b59
tohsaka888 <156132264@qq.com>
2022-05-11 11:14:23 +0800

    feat: 选择区域

---
+++ src/components/SelectArea.tsx
@@ -0,0 +1,27 @@
+import { AreaChartOutlined } from "@ant-design/icons";
+import { Menu, MenuProps } from "antd";
+import React from "react";
+import { SelectAreaContainer } from "../pages/DataCancas.style";
+
+const items: MenuProps["items"] = [
+  {
+    label: "图谱",
+    key: "graph",
+    icon: <AreaChartOutlined />,
+  },
+];
+
+function SelectArea() {
+  return (
+    <SelectAreaContainer>
+      <Menu
+        items={items}
+        defaultSelectedKeys={["graph"]}
+        mode="horizontal"
+        style={{ background: "transparent" }}
+      />
+    </SelectAreaContainer>
+  );
+}
+
+export default SelectArea;
