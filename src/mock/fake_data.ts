d8fc09434d136cbe9cd6ac49cbb7d9730ce76fbb
tohsaka888 <156132264@qq.com>
2022-05-11 14:58:55 +0800

    feat: mock数据

---
+++ src/mock/fake_data.ts
@@ -0,0 +1,160 @@
+const centerPoint = {
+  name: '资产'
+}
+
+const defaultPoint = [
+  {
+    name: '表',
+    id: '0'
+  },
+  {
+    name: '系统',
+    id: '1'
+  },
+  {
+    name: '部门',
+    id: '2'
+  },
+  {
+    name: '库',
+    id: '3'
+  },
+  {
+    name: '资产',
+    id: '4'
+  },
+  {
+    name: '资产',
+    id: '5'
+  }
+]
+
+const fields = [
+  {
+    name: '男方姓名',
+    id: '0'
+  },
+  {
+    name: '女方姓名',
+    id: '1'
+  },
+  {
+    name: '男方身份证号',
+    id: '2'
+  },
+  {
+    name: '女方身份证号',
+    id: '3'
+  },
+  {
+    name: '婚姻登记日期',
+    id: '4'
+  },
+  {
+    name: '婚姻登记类型',
+    id: '5'
+  },
+  {
+    name: '婚姻登记机关',
+    id: '6'
+  },
+  {
+    name: '婚姻登记备注',
+    id: '7'
+  }
+]
+
+const terms = [
+  {
+    name: '术语1',
+    id: '0'
+  }, {
+    name: '术语2',
+    id: '1'
+  }, {
+    name: '术语3',
+    id: '2'
+  }, {
+    name: '术语4',
+    id: '3'
+  }, {
+    name: '术语5',
+    id: '4'
+  }
+]
+
+const dataFields = [
+  {
+    name: '数据字段1',
+    id: '0'
+  }, {
+    name: '数据字段2',
+    id: '1'
+  }, {
+    name: '数据字段3',
+    id: '2'
+  }, {
+    name: '数据字段4',
+    id: '3'
+  }, {
+    name: '数据字段5',
+    id: '4'
+  }
+]
+
+const data = [
+  {
+    name: '数据元1',
+    id: '0'
+  }, {
+    name: '数据元2',
+    id: '1'
+  }, {
+    name: '数据元3',
+    id: '2'
+  }
+]
+
+const codeTables = [
+  {
+    name: '代码表1',
+    id: '0'
+  }, {
+    name: '代码表2',
+    id: '1'
+  }, {
+    name: '代码表3',
+    id: '2'
+  }
+]
+
+const models = [
+  {
+    name: '模型1',
+    id: '0'
+  }, {
+    name: '模型2',
+    id: '1'
+  }, {
+    name: '模型3',
+    id: '2'
+  }
+]
+
+const modelProperties = [
+  {
+    name: '模型属性1',
+    id: '0'
+  }, {
+    name: '模型属性2',
+    id: '1'
+  }, {
+    name: '模型属性3',
+    id: '2'
+  }, {
+    name: '模型属性4',
+    id: '3'
+  }
+]
+
+export { centerPoint, defaultPoint, fields, terms, dataFields, data, codeTables, modelProperties, models }
