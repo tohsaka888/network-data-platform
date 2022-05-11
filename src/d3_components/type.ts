41099dec6583c9a41b1220b955e34da6fecdc01d
tohsaka888 <156132264@qq.com>
2022-05-11 16:50:40 +0800

    update: 更新type

--- src/d3_components/type.ts
+++ src/d3_components/type.ts
@@ -1,8 +1,8 @@
 export type D3CANVAS = d3.Selection<SVGGElement, unknown, null, undefined> | undefined
-export type MOVEPOINT = {
-  x: number;
-  y: number;
-  pointId: string;
+export type POINT = {
+  x: number | string;
+  y: number | string;
+  pointId?: string;
 }
 export type Point = {
   name: string;
