e75d10fc5324f53c32b3d43becc22224f088194e
tohsaka888 <156132264@qq.com>
2022-05-12 14:24:59 +0800

    feat: 增加箭头

--- src/d3_components/line.ts
+++ src/d3_components/line.ts
@@ -4,18 +4,52 @@ import { D3CANVAS, Point, POINT } from "./type"
 
 const drawLine = (container: D3CANVAS, startPoint: POINT | Point, endPoints: Point[]) => {
   endPoints.forEach(item => {
-    container?.insert('path', ':first-child')
+    const arrowSvg = container?.insert('svg', ':first-child')
+    if (item) {
+      arrowSvg?.append('defs')
+        .append('marker')
+        .attr('id', 'arrow')
+        .attr('viewBox', '0 -5 10 12')
+        .attr('refX', (item.x || 0 - +(startPoint.x || 0)) * 0.5)
+        .attr('refY', '0')
+        .attr('markerWidth', '10')
+        .attr('markerHeight', '10')
+        .attr('orient', 'auto')
+        .attr('fill', '#84ADF8')
+        .attr('markerUnits', 'strokeWidth')
+        .append('path')
+        .attr('d', 'M0,-5L10,0L0,5')
+    }
+    arrowSvg?.append('path')
       .attr('d', 'M ' + startPoint.x + ' ' + startPoint.y + ' L ' + item.x + ' ' + item.y)
       .attr('stroke', '#84ADF8')
       .attr('stroke-width', '1px')
+      .attr('marker-end', 'url(#arrow)')
   })
 }
 
 const drawRectLine = (container: D3CANVAS, startPoint: POINT, endPoint: Point) => {
-  container?.insert('path', ':first-child')
+  const arrowSvg = container?.insert('svg', ':first-child')
+  if (startPoint) {
+    arrowSvg?.append('defs')
+      .append('marker')
+      .attr('id', 'arrow')
+      .attr('viewBox', '0 -5 20 12')
+      .attr('refX', (endPoint.x || 0 - +(startPoint.x || 0)) * 0.1)
+      .attr('refY', '0')
+      .attr('markerWidth', '10')
+      .attr('markerHeight', '10')
+      .attr('orient', 'auto')
+      .attr('fill', '#84ADF8')
+      .attr('markerUnits', 'strokeWidth')
+      .append('path')
+      .attr('d', 'M0,-5L10,0L0,5')
+  }
+  arrowSvg?.append('path')
     .attr('d', 'M ' + startPoint.x + ' ' + startPoint.y + ' L ' + endPoint.x + ' ' + endPoint.y)
     .attr('stroke', '#84ADF8')
     .attr('stroke-width', '1px')
+    .attr('marker-end', 'url(#arrow)')
 }
 
 const drawHorizontalLine = (container: D3CANVAS, points: Point[]) => {
