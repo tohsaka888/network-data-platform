8d7da1b470151a7f7332fe77cd95df585a0b2f06
tohsaka888 <156132264@qq.com>
2022-05-12 14:54:31 +0800

    feat: 监听resize事件

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import { Layout } from "antd";
-import React from "react";
+// import * as d3 from "d3";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import DataCanvas from "./pages/DataCanvas";
 
 function App() {
   const { Header, Sider } = Layout;
   const contentHeight = window.innerHeight - 64;
+  const [screenWidth, setScreenWidth] = useState<number>(window.innerWidth);
+  const [screenHeight, setScreenHeight] = useState<number>(window.innerHeight);
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      setScreenWidth(window.innerWidth);
+      setScreenHeight(window.innerHeight);
+    });
+    return () => {
+      window.removeEventListener("resize", () => {});
+    };
+  });
 
   return (
     <div className="App">
@@ -15,7 +27,7 @@ function App() {
           <Sider theme="light" className="layout-sider" />
           <Layout style={{ height: contentHeight }}>
             <Header style={{ background: "white" }} />
-            <DataCanvas />
+            <DataCanvas screenHeight={screenHeight} screenWidth={screenWidth} />
           </Layout>
         </Layout>
       </Layout>
