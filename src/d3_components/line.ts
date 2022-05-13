4d3eb8abac0db350e1e113558d4cf96459a31619
tohsaka888 <156132264@qq.com>
2022-05-13 09:21:03 +0800

    feat: 只创建一个箭头

--- src/d3_components/line.ts
+++ src/d3_components/line.ts
@@ -2,14 +2,12 @@ import * as d3 from "d3"
 import { D3CANVAS, Point, POINT } from "./type"
 // import * as d3 from 'd3'
 
-let arrowSvg: any = null
-
-const drawArraw = (container: D3CANVAS) => {
-  arrowSvg =  container?.insert('defs', ':first-child')
+const drawArraw = () => {
+  d3.selectAll('svg').insert('defs', ':first-child')
     .append('marker')
     .attr('id', 'arrow')
-    .attr('viewBox', '0 -5 10 12')
-    .attr('refX', 10)
+    .attr('viewBox', '0 -5 10 15')
+    .attr('refX', '0')
     .attr('refY', '0')
     .attr('markerWidth', '10')
     .attr('markerHeight', '10')
@@ -22,35 +20,32 @@ const drawArraw = (container: D3CANVAS) => {
 
 const drawLine = (container: D3CANVAS, startPoint: POINT | Point, endPoints: Point[]) => {
   endPoints.forEach(item => {
-    arrowSvg?.append('path')
-      .attr('d', 'M ' + startPoint.x + ' ' + startPoint.y + ' L ' + item.x + ' ' + item.y)
+    let midX = null
+    let midY = null
+    if (startPoint.x && startPoint.y && item.x && item.y) {
+      midX = (+startPoint.x + item.x) / 2
+      midY = (+startPoint.y + item.y) / 2
+    }
+    container?.insert('path', ':first-child')
+      .attr('d', 'M ' + startPoint.x + ' ' + startPoint.y + ' L ' + midX + ' ' + midY + ' L ' + item.x + ' ' + item.y)
       .attr('stroke', '#84ADF8')
       .attr('stroke-width', '1px')
-      .attr('marker-end', 'url(#arrow)')
+      .attr('marker-mid', 'url(#arrow)')
   })
 }
 
 const drawRectLine = (container: D3CANVAS, startPoint: POINT, endPoint: Point) => {
-  if (startPoint) {
-    arrowSvg?.append('path')
-      .append('marker')
-      .attr('id', 'arrow')
-      .attr('viewBox', '0 -5 20 12')
-      .attr('refX', (endPoint.x || 0 - +(startPoint.x || 0)) * 0.1)
-      .attr('refY', '0')
-      .attr('markerWidth', '10')
-      .attr('markerHeight', '10')
-      .attr('orient', 'auto')
-      .attr('fill', '#84ADF8')
-      .attr('markerUnits', 'strokeWidth')
-      .append('path')
-      .attr('d', 'M0,-5L10,0L0,5')
-  }
-  arrowSvg?.append('path')
-    .attr('d', 'M ' + startPoint.x + ' ' + startPoint.y + ' L ' + endPoint.x + ' ' + endPoint.y)
+  let midX = null
+    let midY = null
+    if (startPoint.x && startPoint.y && endPoint.x && endPoint.y) {
+      midX = (+startPoint.x + endPoint.x) / 2
+      midY = (+startPoint.y + endPoint.y) / 2
+    }
+  container?.insert('path', ':first-child')
+    .attr('d', 'M ' + startPoint.x + ' ' + startPoint.y + ' L ' + midX + ' ' + midY + ' L ' + endPoint.x + ' ' + endPoint.y)
     .attr('stroke', '#84ADF8')
     .attr('stroke-width', '1px')
-    .attr('marker-end', 'url(#arrow)')
+    .attr('marker-mid', 'url(#arrow)')
 }
 
 const drawHorizontalLine = (container: D3CANVAS, points: Point[]) => {
