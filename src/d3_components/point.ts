98971d25c264100b2cc1f4704e70e0a2710a5eef
tohsaka888 <156132264@qq.com>
2022-05-16 14:23:29 +0800

    feat: 新增隐藏项目

--- src/d3_components/point.ts
+++ src/d3_components/point.ts
@@ -29,6 +29,12 @@ const createRect = (container: D3CANVAS, x: number, y: number, color: string, en
     if (entity.id.includes('asset_field')) {
       rectContainer?.classed('show', true)
     }
+    if (entity.id.includes('codeinfo')) {
+      rectContainer?.classed('show', true)
+    }
+    if (entity.id.includes('data_meta')) {
+      rectContainer?.classed('show', true)
+    }
     rectContainer
       ?.append('xhtml:div')
       .attr('xmlns', 'http://www.w3.org/1999/xhtml')
