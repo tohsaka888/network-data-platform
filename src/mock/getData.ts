3bc4d6d5bb914fbb2ba83c3fc0acd4de2b419d52
tohsaka888 <156132264@qq.com>
2022-05-16 17:14:50 +0800

    feat: 请求失败使用测试数据

--- src/mock/getData.ts
+++ src/mock/getData.ts
@@ -1,40 +1,50 @@
 import { message } from "antd"
-import { Entity } from "../type"
+import { EDGE, Entity } from "../type"
+import fake_data from './fake_data.json'
+
+const formatData = (data: { entities: Entity[], edges: EDGE[] }) => {
+  let centerPoint: Entity[] = []
+  let model: Entity[] = []
+  let defaultPoint: Entity[] = []
+  let property: Entity[] = []
+  let datameta: Entity[] = []
+  let codeInfo: Entity[] = []
+  let terminology: Entity[] = []
+  let assetField: Entity[] = []
+  data.entities.forEach((item: Entity) => {
+    if (item.label === 'model') {
+      model.push(item)
+    } else if (item.label === 'asset') {
+      centerPoint.push(item)
+    } else if (item.label === 'property') {
+      property.push(item)
+    } else if (item.label === 'datameta') {
+      datameta.push(item)
+    } else if (item.label === 'codeinfo') {
+      codeInfo.push(item)
+    } else if (item.label === 'terminology') {
+      terminology.push(item)
+    } else if (item.label === 'assetfield') {
+      assetField.push(item)
+    } else {
+      defaultPoint.push(item)
+    }
+  })
+  return { model, centerPoint, defaultPoint, datameta, codeInfo, terminology, property, assetField, edges: data.edges }
+}
 
 export const getData = async () => {
   try {
     const res = await fetch(`http://172.168.30.12:8010/api/network/asset/getAssetGraph?id=8`)
     const data = await res.json()
-    let centerPoint: Entity[] = []
-    let model: Entity[] = []
-    let defaultPoint: Entity[] = []
-    let property: Entity[] = []
-    let datameta: Entity[] = []
-    let codeInfo: Entity[] = []
-    let terminology: Entity[] = []
-    let assetField: Entity[] = []
-    data.data.entities.forEach((item: Entity) => {
-      if (item.label === 'model') {
-        model.push(item)
-      } else if (item.label === 'asset') {
-        centerPoint.push(item)
-      } else if (item.label === 'property') {
-        property.push(item)
-      } else if (item.label === 'datameta') {
-        datameta.push(item)
-      } else if (item.label === 'codeinfo') {
-        codeInfo.push(item)
-      } else if (item.label === 'terminology') {
-        terminology.push(item)
-      } else if (item.label === 'assetfield') {
-        assetField.push(item)
-      } else {
-        defaultPoint.push(item)
-      }
-    })
-    return { model, centerPoint, defaultPoint, datameta, codeInfo, terminology, property, assetField, edges: data.data.edges }
+    return formatData(data.data)
   } catch (error: any) {
     message.error(error.message)
-    return { model: [], centerPoint: [], defaultPoint: [], datameta: [], codeInfo: [], terminology: [], property: [], assetField: [], edges: [] }
+    // fake_data.data.entities.forEach((item: any) => {
+    //   if (item.id === null) {
+    //     item.id = 'null'
+    //   }
+    // })
+    return formatData(fake_data.data)
   }
 }
