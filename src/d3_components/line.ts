7ead4feb8939555075cb5156d140d1c76b9c0cbd
tohsaka888 <156132264@qq.com>
2022-05-18 16:23:11 +0800

    feat: 静态画线

--- src/d3_components/line.ts
+++ src/d3_components/line.ts
@@ -1,5 +1,5 @@
 import * as d3 from "d3"
-import { EDGE, D3CANVAS } from "../type"
+import { EDGE, D3CANVAS, Entity } from "../type"
 
 const drawArraw = () => {
   d3.selectAll('svg').insert('defs', ':first-child')
@@ -22,6 +22,7 @@ const drawLine = (container: D3CANVAS, edges: EDGE[]) => {
   edges = Array.from(new Set(edges))
   edges.forEach(item => {
     let id = ''
+    item.showArraw = true
     if (!item.fromId) {
       item.fromId = 'null'
     }
@@ -37,33 +38,43 @@ const drawLine = (container: D3CANVAS, edges: EDGE[]) => {
       item.isDraw = isDraw
       if (!isDraw) {
         toX = container?.select('#id' + item.toId).attr('fieldX') || 0
-        toY = +toY + 50
+        toY = +toY + 19
         isDraw = true
       }
     } else if (item.fromId.includes('asset_field') && item.toId.includes('property')) {
-      fromX = +fromX + 12
-      fromY = + fromY + 100
-      toX = +toX + 12
+      fromX = +fromX + 40
+      fromY = + fromY + 38
+      toX = +toX + 40
       id = 'show'
+      item.showArraw = false
     } else if (item.fromId.includes('asset_field') && (item.toId.includes('terminology') || item.toId.includes('codeinfo') || item.toId.includes('data_meta'))) {
-      fromX = +fromX + 12
+      fromX = +fromX + 40
       fromY = + fromY
-      toX = +toX + 12
-      toY = +toY + 100
+      toX = +toX + 40
+      toY = +toY + 38
       id = 'show'
+      item.showArraw = false
     } else if (item.fromId.includes('model') && item.toId.includes('property')) {
-      toX = +toX + 12
-      toY = + toY + 100
+      toX = +toX + 40
+      toY = + toY + 38
     }
     let midX = (+fromX + +toX) / 2
     let midY = (+fromY + +toY) / 2
 
-    if (!item.isDraw) {
+    if (!item.isDraw && item.showArraw) {
       container?.insert('path', ':first-child')
         .attr('d', 'M ' + fromX + ' ' + fromY + ' L ' + midX + ' ' + midY + ' L ' + toX + ' ' + toY)
         .attr('stroke', '#84ADF8')
         .attr('stroke-width', '1px')
         .attr('marker-mid', 'url(#arrow)')
+        .classed(id, true)
+    }
+
+    if (!item.showArraw) {
+      container?.insert('path', ':first-child')
+        .attr('d', 'M ' + fromX + ' ' + fromY + ' L ' + toX + ' ' + toY)
+        .attr('stroke', '#84ADF8')
+        .attr('stroke-width', '1px')
         .classed(id, true)
     }
   })
@@ -73,25 +84,21 @@ const isShowLine = (isShow: boolean) => {
   d3.selectAll('.show').transition().style('display', isShow ? 'block' : 'none').duration(5000)
 }
 
-// const drawRectLine = (container: D3CANVAS, startPoint: POINT, endPoint: Point) => {
-//   let midX = null
-//   let midY = null
-//   if (startPoint.x && startPoint.y && endPoint.x && endPoint.y) {
-//     midX = (+startPoint.x + endPoint.x) / 2
-//     midY = (+startPoint.y + endPoint.y) / 2
-//   }
-//   container?.insert('path', ':first-child')
-//     .attr('d', 'M ' + startPoint.x + ' ' + startPoint.y + ' L ' + midX + ' ' + midY + ' L ' + endPoint.x + ' ' + endPoint.y)
-//     .attr('stroke', '#84ADF8')
-//     .attr('stroke-width', '1px')
-//     .attr('marker-mid', 'url(#arrow)')
-// }
+const drawStaticLine = (container: D3CANVAS, startPoint: Entity, endPoints: Entity[]) => {
+  endPoints.forEach(item => {
+    let midX
+    let midY
+    if (item.x && item.y && startPoint.x && startPoint.y) {
+      midX = (startPoint.x + item.x) / 2
+      midY = (startPoint.y + item.y) / 2
+    }
+    container?.insert('path', ':first-child')
+      .attr('d', 'M ' + startPoint.x + ' ' + startPoint.y + ' L ' + midX + ' ' + midY + ' L ' + item.x + ' ' + item.y)
+      .attr('stroke', '#84ADF8')
+      .attr('stroke-width', '1px')
+      .classed('show', true)
+      .attr('marker-mid', 'url(#arrow)')
+  })
+}
 
-// const drawHorizontalLine = (container: D3CANVAS, points: Point[]) => {
-//   container?.insert('path', ':first-child')
-//     .attr('d', 'M ' + points[0].x + ' ' + points[0].y + ' L ' + points[points.length - 1].x + ' ' + points[points.length - 1].y)
-//     .attr('stroke', '#84ADF8')
-//     .attr('stroke-width', '1px')
-// }
-
-export { drawLine, drawArraw, isShowLine }
+export { drawLine, drawArraw, isShowLine, drawStaticLine }
